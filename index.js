4bab9eabb73eb59fe4551a61058d90bc4d46b324
Hassan Ogunniyi <ogunniyi95@gmail.com>
2022-01-12 23:36:32 +0100

    Code cleanup

--- index.js
+++ index.js
@@ -22,41 +22,6 @@ settings.addEventListener('click', showSettings);
 
 closeSettings.addEventListener('click', hideSettings);
 
-// function initialPopup(){
-//     gsap.set(popUp,{scale:0});
-
-// };
-
-// initialPopup();
-
-
-
-// function popping(){
-//     gsap.fromTo(popUp,{
-//         opacity:0, scale: 0
-//     },{
-//         opacity: 1, scale: 1, delay:2, duration:0.4, ease: Power1.easeOut
-//     });
-// }
-
-// function popOut(){
-//     gsap.to(popUp,{
-//         opacity:0, scale:0, duration:0.3, ease: Power1.easeOut
-//     });
-// };
-
-// settings.addEventListener('click', openSettings);
-
-// function openSettings(){
-//     gsap.fromTo(settings,{
-//         rotation: '0deg' 
-//     },{
-//         rotation: '360deg', duration: 2, ease: Power1.easeOut
-//     });
-
-//     popping()
-// }
-
 
 const getPhotos = async (photos) => {
 
@@ -106,37 +71,7 @@ const getCrypto = async (crypto) => {
 
 getCrypto(crypto);
 
-// fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=${photos}`)
-//     .then(res => res.json())
-//     .then(data => {
-//         document.body.style.backgroundImage = `url(${data.urls.full})`
-// 		document.getElementById("author").textContent = `By: ${data.user.name}`
-//     })
-//     .catch(err => {
-       
-//         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
-// 		document.getElementById("author").textContent = `By: Dodi Achmad`
-//     })
 
-// fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-//     .then(res => {
-//         if (!res.ok) {
-//             throw Error("Something went wrong")
-//         }
-//         return res.json()
-//     })
-//     .then(data => {
-//         document.getElementById("crypto-top").innerHTML = `
-//             <img src=${data.image.small} />
-//             <span>${data.name}</span>
-//         `
-//         document.getElementById("crypto").innerHTML += `
-//             <p>🎯: $${data.market_data.current_price.usd}</p>
-//             <p>👆: $${data.market_data.high_24h.usd}</p>
-//             <p>👇: $${data.market_data.low_24h.usd}</p>
-//         `
-//     })
-//     .catch(err => console.error(err))
 
 function getCurrentTime() {
     const date = new Date()
@@ -148,27 +83,7 @@ setInterval(getCurrentTime, 1000);
 
 
 
-// navigator.geolocation.getCurrentPosition(position =>{
-//    async() =>{
-//         const base = 'https://apis.scrimba.com/openweathermap/data/2.5/weather'
-//         const query = `?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
-    
-//         try{
-//             const response = await fetch(base + query);
-//             const data = response.json();
-//             console.log(data)
-//             const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-//             document.getElementById("weather").innerHTML = `
-//                 <img src=${iconUrl} />
-//                 <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-//                 <p class="weather-city">${data.name}</p>
-//             `
-//         } catch(error){
-//             console.log(error);
-            
-//         }
-//     }
-// })
+
 
 
 navigator.geolocation.getCurrentPosition(position => {
